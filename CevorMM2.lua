4f8ca9d90c242e1cc86b5e74d15cdbadb15ac2f8
THEB <Worldroleplayman@gmail.com>
2025-03-07 18:18:12 -0500

    CevorMM2.lua

---
+++ CevorMM2.lua
@@ -0,0 +1,2 @@
+getgenv().autocopykey = false -- For Copying link set to true if you already have the link or the key copied and you wanna rerun the script set it to false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/THEBWARE/-/refs/heads/main/CevorMM2V8.3.lua", true))()
